0bd993e2bb1fd13290c0d8933f176d7198261d77
Álvaro José Agámez Licha <alvaroagamez@outlook.com>
2021-07-27 22:06:48 -0500

    Remove unnecessary code

--- src/getFiles.js
+++ src/getFiles.js
@@ -12,22 +12,6 @@ const getFiles = async (dir, pattern) => {
       return resolve(files)
     })
   })
-
-  // const dirents = await readdir(dir, { withFileTypes: true })
-
-  // const files = await Promise.all(dirents.map((dirent) => {
-  //   const resourcePath = resolve(dir, dirent.name)
-
-  //   if (dirent.isDirectory()) {
-  //     return getFiles(resourcePath, extension)
-  //   }
-
-  //   if (dirent.name.endsWith(extension)) {
-  //     return resourcePath
-  //   }
-  // }))
-
-  // return Array.prototype.concat(...files.filter(file => file !== undefined)) // flat the array
 }
 
 module.exports = {
